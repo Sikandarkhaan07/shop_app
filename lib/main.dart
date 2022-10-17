4e3e5f4e6af9fe395a7c440eca2e29a1769ebe5d
Sikandar khan <sikandarkhaan074@gmail.com>
2022-10-17 00:44:21 -0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Shop App'),
+      ),
+      body: const Center(
+        child: Text('Let\'s have a products.'),
+      ),
+    );
+  }
+}
