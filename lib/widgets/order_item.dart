50d3ae4145cee396b486b7167ddc40761a30da26
Sikandar khan <sikandarkhaan074@gmail.com>
2022-10-26 11:00:12 -0700

    now i have added some new features to this app that expand my knowledge and understand how to use suitable resourses in a correct place.

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -1,23 +1,72 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import '../providers/orders.dart' as objectOrder;
 
-class OrderItem extends StatelessWidget {
+class OrderItem extends StatefulWidget {
   final objectOrder.OrderItem order;
+
   OrderItem(this.order);
 
-  
+  @override
+  State<OrderItem> createState() => _OrderItemState();
+}
+
+class _OrderItemState extends State<OrderItem> {
+  var _expanded = false;
+
   @override
   Widget build(BuildContext context) {
     return Card(
+      elevation: 5,
       margin: const EdgeInsets.all(10),
       child: Column(
         children: [
           ListTile(
-            title: Text('\$${order.amount}'),
-            subtitle: Text(DateFormat('dd/MM/yyyy hh:mm').format(order.dateTime),),
-            trailing: IconButton(icon: const Icon(Icons.expand_more), onPressed: (){},),
+            title: Text('\$${widget.order.amount.toStringAsFixed(2)}'),
+            subtitle: Text(
+              DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime),
+            ),
+            trailing: IconButton(
+              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+              onPressed: () {
+                setState(() {
+                  _expanded = !_expanded;
+                });
+              },
+            ),
           ),
+          if (_expanded)
+            Container(
+              color: Colors.black26,
+              height: min(widget.order.products.length * 20.0 + 20, 120),
+              padding: const EdgeInsets.symmetric(
+                vertical: 5,
+                horizontal: 15,
+              ),
+              child: ListView(
+                children: widget.order.products
+                    .map((prod) => Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              prod.title,
+                              style: const TextStyle(
+                                  fontSize: 18, fontWeight: FontWeight.bold),
+                            ),
+                            Text(
+                              '${prod.quantity}x \$${prod.price}',
+                              style: const TextStyle(
+                                fontSize: 18,
+                                color: Colors.white,
+                              ),
+                            )
+                          ],
+                        ))
+                    .toList(),
+              ),
+            ),
         ],
       ),
     );
